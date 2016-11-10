292a7ce8fca73b6ac9b2e61126cf83bcb7d8dd5e
nosanity <no-sanity17@ya.ru>
2016-11-10 19:13:18 +0300

    EM-21 [EM-Payments] Расширить view страницы оплаты для возможности отображения модулей

--- opro_payments/urls.py
+++ opro_payments/urls.py
@@ -5,6 +5,6 @@ from . import views
 
 urlpatterns = [
     url(r'^op_payment/?$', views.op_payment_view, name='op_payment'),
-    url(r'^op_payment/(?P<session_id>\d+)/(?P<user_id>\d+)/(?P<status>success|fail)/?$',
+    url(r'^op_payment/(?P<payment_type>session|edmodule)/(?P<obj_id>\d+)/(?P<user_id>\d+)/(?P<status>success|fail)/?$',
         views.op_payment_status, name='op_payment_status'),
 ]
